bba3b6a0b45b3b3521b245ab24b782fc7dd3576a
Tom Sullivan <tom@msbit.com.au>
2020-03-27 21:12:53 +1100

    Use C++ constructor in C

--- c-caller.c
+++ c-caller.c
@@ -3,7 +3,8 @@
 #include "callee.h"
 
 int main(void) {
-  struct Callee c = { .value = 10 };
+  struct Callee c = {};
+  _ZN6CalleeC1Ei(&c, 10);
   int result = _ZN6Callee9PlusValueEi(&c, 10);
   printf("%d\n", result);
 }
--- callee.h
+++ callee.h
@@ -2,4 +2,5 @@ struct Callee {
   int value;
 };
 
+struct Callee *_ZN6CalleeC1Ei(struct Callee *, int);
 int _ZN6Callee9PlusValueEi(struct Callee *, int);
