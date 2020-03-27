1e089a2ea2714e33da29a4404b65e0f34a8ed458
Tom Sullivan <tom@msbit.com.au>
2020-03-28 00:50:20 +1100

    Add some clang-format updates

--- c-rs-caller.c
+++ c-rs-caller.c
@@ -8,7 +8,7 @@ void *_ZN4core9panicking5panic17h8627f9662a331d41E(void *v) {}
 int _ZN9rs_callee6Callee9PlusValue17h0ce39cb1c4ac843dE(struct Callee *, int);
 
 int main(void) {
-  struct Callee c = { .value = 10 };
+  struct Callee c = {.value = 10};
   int result = _ZN9rs_callee6Callee9PlusValue17h0ce39cb1c4ac843dE(&c, 10);
   printf("%d\n", result);
 }
