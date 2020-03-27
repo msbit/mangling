c18431fb807d0b11911eb51c0f3884ad239b81cd
Tom Sullivan <tom@msbit.com.au>
2020-03-27 21:29:15 +1100

    Remove use of `auto` for older compilers

--- cxx-caller.cpp
+++ cxx-caller.cpp
@@ -3,7 +3,7 @@
 #include "callee.hpp"
 
 int main(void) {
-  auto c = Callee(10);
-  auto result = c.PlusValue(10);
+  Callee c = Callee(10);
+  int result = c.PlusValue(10);
   std::cout << result << std::endl;
 }
